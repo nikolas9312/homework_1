9f1b86770a42157f9fa2b25ddf2fe243ad92825b
nikolas9312 <herrera.juan.nicolas@hotmail.com>
2022-09-30 15:40:29 -0700

    Initial commit

---
+++ git.R
@@ -0,0 +1,10 @@
+library(usethis)
+
+# Configure git username
+use_git_config(user.name = "nikolas9312", user.email = "herrera.juan.nicolas@hotmail.com")
+
+# create token
+usethis::create_github_token()
+
+# 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
+
